f46858cd79277fa250d8cd1022382cedae3c56bb
Piotr Torczynski <piotr.torczynski@netguru.pl>
2017-01-28 21:06:59 +0100

    Added base view class

---
+++ Warsaround/Sources/Views/Abstract/BaseView.swift
@@ -0,0 +1,49 @@
+//
+//  BaseView.swift
+//  Warsaround
+//
+//  Created by Piotr Torczynski on 28/01/2017.
+//  Copyright © 2017 SmartApps. All rights reserved.
+//
+
+import UIKit
+
+internal class BaseView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupProperties()
+        setupHierarchy()
+        setupConstraints()
+        setupBindings()
+    }
+
+    @available(*, unavailable, message: "Use init(frame:) instead")
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Abstract
+
+    /// Sets up the properties of `self`. Called automatically on `init()`.
+    internal func setupProperties() {
+        // no-op by default
+    }
+
+    /// Sets up the view hierarchy of `self`. Called automatically on `init()`.
+    internal func setupHierarchy() {
+        // no-op by default
+    }
+
+    /// Sets up layout constraints in `self`. Called automatically on `init()`.
+    internal func setupConstraints() {
+        // no-op by default
+    }
+
+    /// Sets up the bindings inside `self`. Called automatically on `init()`.
+    internal func setupBindings() {
+        // no-op by default
+    }
+
+}
