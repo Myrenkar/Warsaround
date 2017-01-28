dde191edbf3f6aca80dfde065eb7db9b74085698
Piotr Torczynski <piotr.torczynski@netguru.pl>
2017-01-28 20:59:27 +0100

    Added xcconfig confitguration

---
+++ Warsaround/Sources/Screens/Map/MapView.swift
@@ -0,0 +1,15 @@
+//
+//  MapView.swift
+//  Warsaround
+//
+//  Created by Piotr Torczynski on 28/01/2017.
+//  Copyright © 2017 SmartApps. All rights reserved.
+//
+
+import UIKit
+import PureLayout
+
+
+final class MapView {
+
+}
---
+++ Warsaround/Sources/Screens/Map/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  Warsaround
+//
+//  Created by Piotr Torczynski on 28/01/2017.
+//  Copyright © 2017 SmartApps. All rights reserved.
+//
+
+import UIKit
+
+internal final class MapViewController: UIViewController {
+
+}
